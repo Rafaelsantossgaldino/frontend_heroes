7f5f824833b61db874fd8b0f9116bac51bb00465
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-05 21:08:16 -0300

    refactor component Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,38 @@
+import { Container, HeroCard, CardList } from "./styles" 
+
 export default function Home() {
+
+  const heroes = [
+    {
+      id: 1,
+      title: 'Spider Man',
+      image_url: 'https://static1.purebreak.com.br/articles/9/98/61/9/@/392535--homem-aranha-3-multiverso-teorias-c-950x0-3.jpg'
+    },
+    {
+      id: 2,
+      title: 'Hulk',
+      image_url: 'https://static1.purebreak.com.br/articles/9/98/61/9/@/392535--homem-aranha-3-multiverso-teorias-c-950x0-3.jpg'
+    },
+    {
+      id: 3,
+      title: 'Batman',
+      image_url: 'https://static1.purebreak.com.br/articles/9/98/61/9/@/392535--homem-aranha-3-multiverso-teorias-c-950x0-3.jpg'
+    }
+  ]
+
   return(
-    <div>
-      <h1>Hello Wolrd</h1>
-    </div>
+    <Container>
+      <h1>Heroes</h1>
+      <CardList>
+        {heroes.map(hero => {
+          return (
+            <HeroCard key={hero.id}>
+              <a href="http:google.com.br"><img src={hero.image_url} alt={hero.tile}></img></a>
+              <span>{hero.title}</span>
+            </HeroCard>
+          )
+        })}
+      </CardList>
+    </Container>
   )
 }
